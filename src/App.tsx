0fa8382fdc2e44dfc89a912d7c34bc571c8b352f
Julisan <julisan2911@gmail.com>
2025-12-01 16:59:03 -0300

    Analiticas 1/12

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from './pages/Home';
 import './App.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Analytics />
     </div>
   );
 }
